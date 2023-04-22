c2810d1280f8a4f668c70eba039ac1b4431276a7
Duy Hoang <duyhoang123@hotmail.co.uk>
2023-04-22 15:38:00 +0200

    project finalised

--- src/main/scala/SimpleClient.scala
+++ src/main/scala/SimpleClient.scala
@@ -9,14 +9,18 @@ import org.http4s.implicits.http4sLiteralsSyntax
  */
 
 object SimpleClient extends IOApp{
-  def callEffect(client: Client[IO], str: String): IO[String] =
+  def callEffect(client: Client[IO], str: String): IO[String] = {
+    // note that the client expects a String and if we want a different data type, we have to map it
     client.expect[String](uri"http://localhost:8080/length/" / str)
+  }
 
   override def run(args: List[String]): IO[ExitCode] =
     BlazeClientBuilder[IO].resource
+      // difference between client vs. server syntax, '.use' needed to allocate actual client and supply it to lambda function
       .use { client =>
         // note that the unsafeRunSync brings back memories of Async Run -> so is it related to Asynchronous Ops
         println(callEffect(client, "Baeldung").unsafeRunSync())
+        // client released when below effect is called
         IO.unit
       }
       .as(ExitCode.Success)
